a1874585c160ad9fcfa30714da632b5454e9657d
Adriaan Veney <45220404+aveney@users.noreply.github.com>
2021-07-27 09:26:16 -0400

    Add files via upload

---
+++ src/main/java/com/InventoryApplication/InventoryAuditApplication/InventoryAuditApplication.java
@@ -0,0 +1,13 @@
+package com.InventoryApplication.InventoryAuditApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryAuditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InventoryApplication/InventoryAuditApplication/InventoryAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InventoryApplication.InventoryAuditApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
